2b8a416042b5d1c7eb915829fa8c27e0a6057bc1
Sun Young Bae <tjsdud6677@gmail.com>
2022-08-13 20:48:23 +0900

    add search file

---
+++ src/component/searchpage/Searchpage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import SearchMenu from './SearchMenu';
+import './Searchpage.css'
+
+function Searchpage() {
+    return (
+    <div className="Searchpage_BG">
+        <div className="Searchpage_support">
+            <div>
+                <div className="Searchpage_text">passtival에서<br/>원하는 축제를<br/>찾아보세요</div>
+            </div>
+            <div className="Searchpage_menu_BG">
+                <div className="Serchpage_menu">
+                    <SearchMenu></SearchMenu>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Searchpage;
