3d6a7049caeb41aa6fec34dd75c13d08bca06b58
Sun Young Bae <tjsdud6677@gmail.com>
2022-08-13 20:58:12 +0900

    update Home

---
+++ src/page/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Searchpage from '../../component/searchpage/Searchpage';
+
+function Home() {
+    return (
+        <div>
+            <Searchpage />
+        </div>
+    )
+}
+
+export default Home;
