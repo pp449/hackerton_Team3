1cc70fc0433c99a770fc86099148c1a9472bd3d7
pp449 <mma7710@naver.com>
2022-08-13 21:30:50 +0900

    Add header and main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './page/home/Home';
 
 function App() {
   return (
---
+++ src/page/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Footer from '../../component/common/Footer/Footer';
+import Header from '../../component/common/Header/Header';
+import Sectionpage from '../../component/sectionpage/Sectionpage';
+
+function Home() {
+    return (
+      <div className='text-center'>
+        <Header />
+        <div className="scroll-smooth h-screen snap-y overflow-x-scroll snap-mandatory">
+          <Sectionpage title="현재 진행중인 축제" />
+          <Sectionpage title="곧 진행될 축제" />
+          <Sectionpage title="추천 축제" />
+          <button className='rounded-xl bg-blue-400 w-40 h-10 mb-14 hover:opacity-60'>
+            모든 축제보기
+          </button>
+          <Footer />
+        </div>
+      </div>
+    )
+}
+
+export default Home;
